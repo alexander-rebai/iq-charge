f9d810b02e737fbc7e8537ce56dad7d8442fe3a5
Alexander Rebai <alexander-rebai@hotmail.com>
2024-03-20 11:11:03 +0100

    improve site

--- src/components/offerte-formulier/user-type.tsx
+++ src/components/offerte-formulier/user-type.tsx
@@ -17,37 +17,36 @@ const UserType = () => {
 
   if (userType) {
     onSelectUserType(userType as "particulier" | "bedrijf");
-  }
-
-  return (
-    <div className="flex items-center justify-center gap-8 p-12">
-      <div className="flex flex-col">
-        <CardContent>
-          <div className="flex flex-col items-center gap-4">
-            <h1 className="text-4xl font-extrabold text-primary">Over u</h1>
+  } else
+    return (
+      <div className="flex items-center justify-center gap-8 p-12">
+        <div className="flex flex-col">
+          <CardContent>
+            <div className="flex flex-col items-center gap-4">
+              <h1 className="text-4xl font-extrabold text-primary">Over u</h1>
+            </div>
+          </CardContent>
+          <div className="flex flex-col gap-x-12 gap-y-6 md:flex-row">
+            <Card
+              className="cursor-pointer hover:bg-muted"
+              onClick={() => onSelectUserType("bedrijf")}
+            >
+              <CardContent className="flex h-16 w-40 items-center justify-center p-4">
+                Bedrijf
+              </CardContent>
+            </Card>
+            <Card
+              className="cursor-pointer hover:bg-muted"
+              onClick={() => onSelectUserType("particulier")}
+            >
+              <CardContent className="flex h-16 w-40 items-center justify-center p-4">
+                Particulier
+              </CardContent>
+            </Card>
           </div>
-        </CardContent>
-        <div className="flex flex-col gap-x-12 gap-y-6 md:flex-row">
-          <Card
-            className="cursor-pointer hover:bg-muted"
-            onClick={() => onSelectUserType("bedrijf")}
-          >
-            <CardContent className="flex h-16 w-40 items-center justify-center p-4">
-              Bedrijf
-            </CardContent>
-          </Card>
-          <Card
-            className="cursor-pointer hover:bg-muted"
-            onClick={() => onSelectUserType("particulier")}
-          >
-            <CardContent className="flex h-16 w-40 items-center justify-center p-4">
-              Particulier
-            </CardContent>
-          </Card>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default UserType;
